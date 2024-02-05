ed5d71cb90078eff63a28633d5d20a15fdc669c1
Haris Sohail <harissohail.cs@gmail.com>
2024-02-05 11:51:24 +0500

    Read numbers from numbers.txt
    Read numbers from numbers.txt and store them in a dynamically allocated array

--- serial.cpp
+++ serial.cpp
@@ -3,20 +3,47 @@
 #include <string>
 using namespace std;
 
+size_t getArraySize(const int* arr) {
+    // Assuming the array is terminated by a special value, like -1
+    size_t size = 0;
+    while (arr[size] != -1) {
+        ++size;
+    }
+    return size;
+}
+
+void printIntArray(const int *arr, size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        cout << arr[i] << std::endl;
+    }
+}
+
 void readRollNumbers(FILE *inputFile, int *Numbers, int num)
 {
     char number[256];
+    int numbersI = 0;
+    string number_in_String;
+    int numberI;
 
-    while (fgets(number, sizeof(inputFile), inputFile) != nullptr) {
-        cout << number;
+    // read the file and add numbers to the array
+    while (fgets(number, sizeof(inputFile), inputFile) != nullptr)
+    {
+        number_in_String = number;
+        numberI = stoi(number_in_String);
+        Numbers[numbersI++] = numberI;
     }
+    Numbers[numbersI] = -1;
+
+    printIntArray(Numbers, getArraySize(Numbers));
 }
 
 int main()
 {
     FILE *numbersFile = fopen("./assets/numbers.txt", "r");
 
-    int *numbers;
+    int *numbers = new int[1000];
     int num;
 
     readRollNumbers(numbersFile, numbers, num);

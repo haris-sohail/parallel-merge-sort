9bcdce5063ed8e9f7dfcd1a67221c778df76c0e5
Haris Sohail <harissohail.cs@gmail.com>
2024-02-09 18:54:20 +0500

    Add parallel merge sort functionality

---
+++ mergeInfo.h
@@ -0,0 +1,8 @@
+#include"node.h"
+using namespace std;
+struct mergeInfo
+{
+    node<int> *result;
+    node<int> *left;
+    node<int> *right;
+};
--- util.h
+++ util.h
@@ -5,6 +5,7 @@
 #include "node.h"
 #include <mutex>
 #include "threadInfo.h"
+#include "mergeInfo.h"
 using namespace std;
 
 mutex listMutex;
@@ -185,8 +186,8 @@ void writeNumbersToFile(const string &filename)
         return;
     }
 
-    // Write 100000 random numbers separated by newlines
-    for (int i = 0; i < 1000; ++i)
+    // Write random numbers separated by newlines
+    for (int i = 0; i < 100000; ++i)
     {
         outFile << rand() % 10000 << '\n';
     }
@@ -210,7 +211,7 @@ void executeSerial()
 
     delete[] numbers;
 
-    // head = mergeSort(head);
+    head = mergeSort(head);
 
     // printLinkedList(head);
 
@@ -274,15 +275,21 @@ void setThreadInfo(threadInfo *&threadInfoArray, int *Numbers)
     }
 }
 
-node<int>* mergeLists(node<int>* list1, node<int>* list2) {
-    if (list1 == nullptr) return list2;
-    if (list2 == nullptr) return list1;
+node<int> *mergeLists(node<int> *list1, node<int> *list2)
+{
+    if (list1 == nullptr)
+        return list2;
+    if (list2 == nullptr)
+        return list1;
 
-    node<int>* mergedHead = nullptr;
-    if (list1->val <= list2->val) {
+    node<int> *mergedHead = nullptr;
+    if (list1->val <= list2->val)
+    {
         mergedHead = list1;
         mergedHead->next = mergeLists(list1->next, list2);
-    } else {
+    }
+    else
+    {
         mergedHead = list2;
         mergedHead->next = mergeLists(list1, list2->next);
     }
@@ -304,19 +311,20 @@ void createThreads(node<int> *head, FILE *numbersFile, int *Numbers)
 
     setThreadInfo(threadInfoArray, Numbers);
 
-    for (int i = 0; i <= 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         pthread_create(&input[i], &atr, addRollNumbersToListParallel, &threadInfoArray[i]);
         setAffinity(input[i], i);
     }
 
-    for (int i = 0; i <= 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         pthread_join(input[i], NULL);
     }
 
     // join all linked lists to make one
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++)
+    {
         mergeLists(head, threadInfoArray[i].head);
     }
 }
@@ -329,8 +337,278 @@ void removeAtHead(node<int> *&head)
     delete temp;
 }
 
-void* mergeSortParallel(void* arg){
-    
+void divideIntoEight(node<int> *head, node<int> *&head1, node<int> *&head2, node<int> *&head3, node<int> *&head4,
+                     node<int> *&head5, node<int> *&head6, node<int> *&head7, node<int> *&head8)
+{
+    if (head == nullptr)
+    {
+        head1 = head2 = head3 = head4 = head5 = head6 = head7 = head8 = nullptr;
+        return;
+    }
+
+    int length = 0;
+    node<int> *current = head;
+
+    // Calculate the length of the linked list
+    while (current != nullptr)
+    {
+        length++;
+        current = current->next;
+    }
+
+    // Calculate the size of each portion
+    int portionSize = length / 8;
+    int remainder = length % 8;
+
+    current = head;
+    head1 = current;
+
+    // Move to the end of the first portion
+    for (int i = 1; i < portionSize; ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the first portion and update head2
+    head2 = current->next;
+    current->next = nullptr;
+    current = head2;
+
+    // Move to the end of the second portion
+    for (int i = 1; i < portionSize + (remainder > 0 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the second portion and update head3
+    head3 = current->next;
+    current->next = nullptr;
+    current = head3;
+
+    // Move to the end of the third portion
+    for (int i = 1; i < portionSize + (remainder > 1 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the third portion and update head4
+    head4 = current->next;
+    current->next = nullptr;
+    current = head4;
+
+    // Move to the end of the fourth portion
+    for (int i = 1; i < portionSize + (remainder > 2 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the fourth portion and update head5
+    head5 = current->next;
+    current->next = nullptr;
+    current = head5;
+
+    // Move to the end of the fifth portion
+    for (int i = 1; i < portionSize + (remainder > 3 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the fifth portion and update head6
+    head6 = current->next;
+    current->next = nullptr;
+    current = head6;
+
+    // Move to the end of the sixth portion
+    for (int i = 1; i < portionSize + (remainder > 4 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the sixth portion and update head7
+    head7 = current->next;
+    current->next = nullptr;
+    current = head7;
+
+    // Move to the end of the seventh portion
+    for (int i = 1; i < portionSize + (remainder > 5 ? 1 : 0); ++i)
+    {
+        current = current->next;
+    }
+
+    // Split the seventh portion and update head8
+    head8 = current->next;
+    current->next = nullptr;
+}
+
+void setMergeInfo(mergeInfo *&mergeInfoArray, node<int> *head1, node<int> *head2,
+                  node<int> *head3, node<int> *head4, node<int> *head5,
+                  node<int> *head6, node<int> *head7, node<int> *head8)
+{
+    mergeInfoArray[0].left = head1;
+    mergeInfoArray[0].right = head2;
+
+    mergeInfoArray[1].left = head3;
+    mergeInfoArray[1].right = head4;
+
+    mergeInfoArray[2].left = head5;
+    mergeInfoArray[2].right = head6;
+
+    mergeInfoArray[3].left = head7;
+    mergeInfoArray[3].right = head8;
+}
+
+void setMergeInfo_1(mergeInfo *&mergeInfoArray, node<int> *head1, node<int> *head2,
+                    node<int> *head3, node<int> *head4)
+{
+    mergeInfoArray[0].left = head1;
+    mergeInfoArray[0].right = head2;
+
+    mergeInfoArray[1].left = head3;
+    mergeInfoArray[1].right = head4;
+}
+
+node<int> *sort(node<int> *left, node<int> *right)
+{
+    node<int> *dummyHead = new node<int>();
+    node<int> *current = dummyHead;
+
+    while (left != nullptr && right != nullptr)
+    {
+        if (left->val <= right->val)
+        {
+            current->next = left;
+            left = left->next;
+        }
+        else
+        {
+            current->next = right;
+            right = right->next;
+        }
+        current = current->next;
+    }
+
+    // Append remaining elements if any
+    if (left != nullptr)
+    {
+        current->next = left;
+    }
+    else
+    {
+        current->next = right;
+    }
+
+    // Return the head of the merged sorted list
+    node<int> *sortedList = dummyHead->next;
+    delete dummyHead; // Free the dummy head node
+    return sortedList;
+}
+
+void *mergeSortParallelThread(void *arg)
+{
+    mergeInfo *mergeInfoObj = (mergeInfo *)arg;
+
+    node<int> *left = mergeInfoObj->left;
+
+    node<int> *right = mergeInfoObj->right;
+
+    // sort left and right and then merge them
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    node<int> *newHead = merge(left, right);
+
+    mergeInfoObj->result = newHead;
+}
+
+void createThreadsSort(mergeInfo *&mergeInfoArray, node<int> *head, node<int> *head1, node<int> *head2, node<int> *head3, node<int> *head4,
+                       node<int> *head5, node<int> *head6, node<int> *head7, node<int> *head8)
+{
+    pthread_t merge[4];
+
+    pthread_attr_t atr;
+
+    pthread_attr_init(&atr);
+
+    // making thread joinable
+    pthread_attr_setdetachstate(&atr, PTHREAD_CREATE_JOINABLE);
+
+    setMergeInfo(mergeInfoArray, head1, head2, head3, head4, head5, head6, head7, head8);
+
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_create(&merge[i], &atr, mergeSortParallelThread, &mergeInfoArray[i]);
+        setAffinity(merge[i], i);
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_join(merge[i], NULL);
+    }
+}
+
+void createThreadsSort_1(mergeInfo *&mergeInfoArray, node<int> *head1, node<int> *head2,
+                         node<int> *head3, node<int> *head4)
+{
+    pthread_t merge[4];
+
+    pthread_attr_t atr;
+
+    pthread_attr_init(&atr);
+
+    // making thread joinable
+    pthread_attr_setdetachstate(&atr, PTHREAD_CREATE_JOINABLE);
+
+    setMergeInfo_1(mergeInfoArray, head1, head2, head3, head4);
+
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_create(&merge[i], &atr, mergeSortParallelThread, &mergeInfoArray[i]);
+        setAffinity(merge[i], i);
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_join(merge[i], NULL);
+    }
+}
+
+node<int> *mergeSortParallel(node<int> *head)
+{
+    mergeInfo *mergeInfoArray = new mergeInfo[4];
+    mergeInfo *mergeInfoArray_1 = new mergeInfo[2];
+
+    if (head->next == NULL)
+    {
+        return head;
+    }
+
+    // divide list into eight parts
+    node<int> *head1 = new node<int>;
+    node<int> *head2 = new node<int>;
+    node<int> *head3 = new node<int>;
+    node<int> *head4 = new node<int>;
+    node<int> *head5 = new node<int>;
+    node<int> *head6 = new node<int>;
+    node<int> *head7 = new node<int>;
+    node<int> *head8 = new node<int>;
+
+    divideIntoEight(head, head1, head2, head3, head4, head5, head6, head7, head8);
+
+    // combine 8 lists into 4 sorted lists
+
+    createThreadsSort(mergeInfoArray, head, head1, head2, head3, head4, head5, head6, head7, head8);
+
+    // combine 4 lists into 2 sorted lists
+
+    createThreadsSort_1(mergeInfoArray_1, mergeInfoArray[0].result, mergeInfoArray[1].result,
+                        mergeInfoArray[2].result, mergeInfoArray[3].result);
+
+    // combine 2 lists into a single list
+
+    node<int> *newHead = merge(mergeInfoArray_1[0].result, mergeInfoArray_1[1].result);
+
+    return newHead;
 }
 
 void executeParallel()
@@ -347,11 +625,15 @@ void executeParallel()
     createThreads(head, numbersFile, numbers);
 
     // head contains redundant zeros
-    removeAtHead(head); 
-    removeAtHead(head); 
-    removeAtHead(head); 
-    removeAtHead(head); 
-    removeAtHead(head); 
-    
+    removeAtHead(head);
+    removeAtHead(head);
+    removeAtHead(head);
+    removeAtHead(head);
+    removeAtHead(head);
+
+    head = mergeSortParallel(head);
+
+    // printLinkedList(head);
+
     writeResultToFile(head, "./assets/result.txt");
 }
